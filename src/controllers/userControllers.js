7fae5b0e1ad16f48d1bd86222946e3e59a443bdd
ale seravalle <143661004+aleseravalle@users.noreply.github.com>
2024-03-05 14:53:32 -0300

    configure

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -31,7 +31,7 @@ const controller = {
                 image: req.file?.filename || "default-image.png",
                 roles_id: role.id
             };
-            console.log(newUser);
+            //console.log(newUser);
             const existingUser = await db.User.findOne({where: {email: req.body.email}}); 
                 if (existingUser) {
                     return res.render("register", {
